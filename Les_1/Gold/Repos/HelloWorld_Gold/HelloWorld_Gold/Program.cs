54fafbfe0a5faca6acb8282e5e4f44e42b0404d6
yairme <yairstward2013@gmail.com>
2021-02-15 15:14:39 +0100

    First commit

---
+++ Les_1/Gold/Repos/HelloWorld_Gold/HelloWorld_Gold/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace HelloWorld_Gold
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string text = ("Hello World!!!"); 
+            
+            Console.ForegroundColor
+             = ConsoleColor.Green;
+
+            foreach (char c in text)
+            {
+                Console.Write(c);
+                Thread.Sleep(100);
+            }
+
+        }
+    }
+}
---
+++ Les_1/Silver/Repos/HelloWorld_Silver/HelloWorld_Silver/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld_Silver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor
+            = ConsoleColor.Green;
+
+            Console.WriteLine("Hello World!",
+                Console.ForegroundColor);
+        }
+    }
+}
